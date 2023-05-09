f3efc217eb53c9e47b31bb5594d6c57ada25cdda
GaOn <xnoag@icloud.com>
2023-05-09 17:40:11 +0900

    Initial Commit

---
+++ MC2_BuriBuri/MC2_BuriBuriApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC2_BuriBuriApp.swift
+//  MC2_BuriBuri
+//
+//  Created by xnoag on 2023/05/09.
+//
+
+import SwiftUI
+
+@main
+struct MC2_BuriBuriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
